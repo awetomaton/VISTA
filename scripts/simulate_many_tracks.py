4b77f82330dbf2d7b05e99b5084bdf8404560fff
Stephen Hartzell <hartzell.stephen@gmail.com>
2025-11-02 11:47:12 -0500

    Added data loader to version control

---
+++ scripts/simulate_many_tracks.py
@@ -0,0 +1,29 @@
+import h5py
+import numpy as np
+import pathlib
+from vista.simulate.simulation import Simulation
+import plotly.express as px
+
+
+DATA_DIR = pathlib.Path(__file__).resolve().parent.parent / "data"
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def simulate_scenario():
+    simulation = Simulation(name="Made Up")
+    simulation.num_trackers = 3
+    simulation.num_tracks_range = (20, 40)
+    simulation.simulate()
+    basic_scenario_dir = DATA_DIR / "many_tracks"
+    basic_scenario_dir.mkdir(exist_ok=True)
+    simulation.save(basic_scenario_dir)
+    
+    with h5py.File(R"C:\Users\Stephen Hartzell\projects\vista\data\many_tracks\imagery.h5", "r") as fid:
+        datacube = fid["images"][:]
+    px.imshow(np.max(datacube, axis=0), color_continuous_scale="Bluered").show()
+
+
+
+
+if __name__ == "__main__":
+    simulate_scenario()
