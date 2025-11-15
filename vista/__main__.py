5ee0fc148a2e43d667486c3fbb2a519040a7b08e
Stephen Hartzell <hartzell.stephen@gmail.com>
2025-11-15 13:00:03 -0500

    Fixed bug where running VISTA from the __main__.py transposed the imagery

--- vista/__main__.py
+++ vista/__main__.py
@@ -5,6 +5,7 @@ This module allows the VISTA application to be run as a package:
     python -m vista
 """
 import sys
+import pyqtgraph as pg
 from PyQt6.QtWidgets import QApplication
 
 from vista.widgets.core.main_window import VistaMainWindow
@@ -13,6 +14,10 @@ from vista.widgets.core.main_window import VistaMainWindow
 def main():
     """Launch the VISTA application."""
     app = QApplication(sys.argv)
+
+    # Set pyqtgraph configuration
+    pg.setConfigOptions(imageAxisOrder='row-major')
+
     window = VistaMainWindow()
     window.show()
     sys.exit(app.exec())
