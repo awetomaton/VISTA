a7d93c54bf1950878ac6421b8702a3767ec3f18d
Luke Weisenbach <lweisenbach@awetomaton.com>
2026-08-14 15:02:08 -0400

    add subclasses to module

--- vista/known_sources/__init__.py
+++ vista/known_sources/__init__.py
@@ -2,3 +2,10 @@
 
 This module provides positions of known sources that might be in the images.
 """
+
+from vista.known_sources.known_sources import KnownSources
+from vista.known_sources.satellites import Satellites
+from vista.known_sources.solar_system_bodies import SolarSystemBodies
+from vista.known_sources.stars import Stars
+
+__all__ = ["KnownSources", "Satellites", "SolarSystemBodies", "Stars"]
